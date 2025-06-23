f9c60ebfc6f555d79990d19bcd776a50e9dc59d5
Amit Talole <atalole@woyce.io>
2025-06-24 01:43:50 +0530

    remove_dotenv

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import 'dotenv/config';
+// import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
