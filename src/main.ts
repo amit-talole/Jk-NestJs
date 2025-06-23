7d5ddd8664e8bce82b225118fd0bb10cd4a62499
Amit Talole <atalole@woyce.io>
2025-06-24 01:38:09 +0530

    dot env fix

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
